4f8ae67352512d03c29d40f1df50ed16f344afa5
Nico_Zhang <2091571762@qq.com>
2017-08-30 19:39:40 +0800

    first day

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports={
+    AE:{
+        // URL: "http://api.sandbox.guoran100.com:8080/api",
+        // URL: "http://api.guoran100.com:9001/api",
+        URL: "http://192.168.1.6:8080/api",
+        AEAPI_APPKEY:'609388a15b3dfaca', //API AppKey
+        AEAPI_MASTERKEY:'1292b2d414d45c8f97d44354de24c40c', //AppMasterKey
+    },
+    
+}
